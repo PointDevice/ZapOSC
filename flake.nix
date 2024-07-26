3699922ff938c3750979c5c309ba060d0cf7a054
PointDevice <rockit1225@gmail.com>
2024-07-26 02:32:00 -0600

    add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in rec {
+      devShell = pkgs.mkShell {
+        name = "surway-shell";
+        buildInputs = with pkgs; [
+          meson
+          ninja
+          gcc
+          pkg-config
+          gtk3
+        ];
+      };
+    }
+  );
+}
